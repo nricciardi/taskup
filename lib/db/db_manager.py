5329b57eb839f44416393766b2c375817b1af89a
ncla <ricciardincl@gmail.com>
2023-03-18 15:55:19 +0100

    Added sqlite3 connection

--- lib/db/db_manager.py
+++ lib/db/db_manager.py
@@ -1,4 +1,9 @@
+import sqlite3
+
+
 class DBManager:
-    def __int__(self):
-        pass
+    def __int__(self, db_name: str):
+        self.db_name = db_name
+        self.db_connection = sqlite3.connect(self.db_name)
+        self.db_cursor = self.db_connection.cursor()
 
