ccb5caca1189ed7aacdbb2b7160369c9e492476a
ncla <ricciardincl@gmail.com>
2023-03-20 21:43:31 +0100

    Added new settings

--- lib/settings/settings_base.py
+++ lib/settings/settings_base.py
@@ -3,16 +3,20 @@ from lib.utils.base import Base
 
 
 class SettingsBase:
-
+    WORK_DIRECTORY_NAME = "work"
     SETTINGS_FILE_NAME = "settings.json"
 
     DB_NAME_KEY = "db_name"
     BASE_DB_NAME_VALUE = "database.db"
 
+    VERBOSE_KEY = "verbose"
+    BASE_VERBOSE_VALUE = True
+
     PROJECT_PATH_KEY = "project_path"
 
     BASE_SETTINGS = {
-        DB_NAME_KEY: BASE_DB_NAME_VALUE
+        DB_NAME_KEY: BASE_DB_NAME_VALUE,
+        VERBOSE_KEY: BASE_VERBOSE_VALUE
     }
 
     @staticmethod
