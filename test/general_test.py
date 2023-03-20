8298d0a88c55b3cfebce32e09d420fd70cddf8c9
ncla <ricciardincl@gmail.com>
2023-03-20 21:43:18 +0100

    Test

--- test/general_test.py
+++ test/general_test.py
@@ -1,16 +1,7 @@
-from lib.settings.settings_manager import SettingsManager
-from lib.utils.base import Base
+from lib.entity.user import UserManager
 
-
-class Punto:
-    def __init__(self):
-        self.x = 0
-        self.y = 0
-        self.z = 1
 
 
 if __name__ == '__main__':
 
-    p = Punto()
-
-    print(p.z)
+    user_manager = UserManager()
