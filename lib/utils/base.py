9002cc54ada2e0037d68927a7ae4afb347ef437e
ncla <ricciardincl@gmail.com>
2023-03-25 11:37:34 +0100

    Bug fixing and improvement

--- lib/utils/base.py
+++ lib/utils/base.py
@@ -3,12 +3,17 @@ import pathlib
 import sys
 import colorama
 from colorama import Fore, Back, Style
+import traceback
+
 
 # Initialising Colorama (Important)
 colorama.init(autoreset=True)
 
 class Base:
+    """
+    Base class contains utilities
 
+    """
 
     @staticmethod
     def exit():
@@ -30,14 +35,25 @@ class Base:
         return str(project_path)
 
     @staticmethod
-    def log_error(error):
-        print(f"{Fore.RED}ERROR: {error}")
+    def log_error(error, full: bool = False):
+
+        error = f"{Fore.RED}ERROR: {error}"
+
+        if full:
+            exc_type, exc_value, exc_traceback = sys.exc_info()
+            line_num = exc_traceback.tb_lineno
+            file_name = traceback.extract_tb(exc_traceback)[-1][0]
+
+            error += f" -> line {line_num} in {file_name}"
+
+        print(error)
+
 
     @staticmethod
     def log_success(success: str):
-        print(f"{Fore.GREEN}ERROR: {success}")
+        print(f"{Fore.GREEN}SUCCESS: {success}")
 
     @staticmethod
     def log_info(info: str):
-        print(f"{Fore.CYAN}ERROR: {info}")
+        print(f"{Fore.CYAN}INFO: {info}")
 
