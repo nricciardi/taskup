bb7122574aa99147e0622f83c476c3e27dab2420
ncla <ricciardincl@gmail.com>
2023-03-25 15:06:10 +0100

    Added is_verbose to prevent if verbose

--- lib/utils/base.py
+++ lib/utils/base.py
@@ -4,10 +4,11 @@ import sys
 import colorama
 from colorama import Fore, Back, Style
 import traceback
-
+from typing import Any
 
 # Initialising Colorama (Important)
 colorama.init(autoreset=True)
+
 
 class Base:
     """
@@ -35,25 +36,66 @@ class Base:
         return str(project_path)
 
     @staticmethod
-    def log_error(error, full: bool = False):
+    def log_error(message: Any, full: bool = False, is_verbose: bool = True) -> None:
+        """
+        Log pre-formatted error
 
-        error = f"{Fore.RED}ERROR: {error}"
+        :param message: message to print
+        :type message: Any
+        :param full: include trace bock
+        :type full: bool
+        :param is_verbose: it used to check if it is verbose
+        :type is_verbose: bool
+
+        :return: None
+        """
+
+        if not is_verbose:
+            return
+
+        exc_type, exc_value, exc_traceback = sys.exc_info()
+        line_num = exc_traceback.tb_lineno
+        file_name = traceback.extract_tb(exc_traceback)[-1][0]
+
+        message = f"{Fore.RED}ERROR: {str(message)} -> line {line_num} in {file_name}"
 
         if full:
-            exc_type, exc_value, exc_traceback = sys.exc_info()
-            line_num = exc_traceback.tb_lineno
-            file_name = traceback.extract_tb(exc_traceback)[-1][0]
+            tb_str = traceback.format_exc()
+            message += f"\nFull traceback: {tb_str}"
 
-            error += f" -> line {line_num} in {file_name}"
-
-        print(error)
-
+        print(message)
 
     @staticmethod
-    def log_success(success: str):
-        print(f"{Fore.GREEN}SUCCESS: {success}")
+    def log_success(message: Any, is_verbose: bool = True) -> None:
+        """
+        Log pre-formatted success
+
+        :param message: message to print
+        :type message: Any
+        :param is_verbose: it used to check if it is verbose
+        :type is_verbose: bool
+
+        :return: None
+        """
+
+        if not is_verbose:
+            return
+
+        print(f"{Fore.GREEN}SUCCESS: {str(message)}")
 
     @staticmethod
-    def log_info(info: str):
-        print(f"{Fore.CYAN}INFO: {info}")
+    def log_info(message: Any, is_verbose: bool = True):
+        """
+        Log pre-formatted info
 
+        :param message: info to print
+        :type message: Any
+        :param is_verbose: it used to check if it is verbose
+        :type is_verbose: bool
+
+        :return: None
+        """
+        if not is_verbose:
+            return
+
+        print(f"{Fore.CYAN}INFO: {str(message)}")
