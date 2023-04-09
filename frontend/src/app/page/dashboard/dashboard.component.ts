b74225d79ba3a1cd61a3e6710f4ad05b86488e2d
ncla <ricciardincl@gmail.com>
2023-04-09 17:19:59 +0200

    Bug fixing

--- frontend/src/app/page/dashboard/dashboard.component.ts
+++ frontend/src/app/page/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { TaskService } from 'src/app/service/api/task/task.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -7,4 +8,19 @@ import { Component } from '@angular/core';
 })
 export class DashboardComponent {
 
+  constructor(private taskService: TaskService) {
+  }
+
+  ngOnInit() {
+
+    this.taskService.all().then((response) => {
+      response.subscribe({
+        next: (value) => {
+          console.log(value);
+
+        }
+      })
+    })
+
+  }
 }
