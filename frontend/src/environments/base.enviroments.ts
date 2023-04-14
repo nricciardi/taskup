2d4bae113c6df037008eaa64f950e82790c69f22
ncla <ricciardincl@gmail.com>
2023-04-14 23:09:43 +0200

    Added base env

---
+++ frontend/src/environments/base.enviroments.ts
@@ -0,0 +1,8 @@
+export const baseEnvironment = {
+  eelCallRefreshInterval: 1000,
+  fullDateFormat: 'dd/MM/yyyy H:mm',
+  dateFormat: 'dd/MM/yyyy',
+  timeFormat: 'H:mm',
+  warningDateDayBefore: 7,
+  dangerDateDayBefore: 3
+};
