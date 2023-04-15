f6c30cc906161d55654dfb67db23aa247ab0a7c5
ncla <ricciardincl@gmail.com>
2023-04-15 10:33:21 +0200

    Added task order by

--- frontend/src/app/page/dashboard/dashboard.component.ts
+++ frontend/src/app/page/dashboard/dashboard.component.ts
@@ -4,6 +4,13 @@ import { TaskStatusModel } from 'src/app/model/entity/task-status.model';
 import { TaskModel } from 'src/app/model/entity/task.model';
 import { DashboardService } from 'src/app/service/api/dashboard/dashboard.service';
 
+
+enum OrderBy {
+  PRIORITY = "priority",
+  DEADLINE = "deadline"
+}
+
+
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
@@ -11,9 +18,31 @@ import { DashboardService } from 'src/app/service/api/dashboard/dashboard.servic
 })
 export class DashboardComponent {
 
+  OrderBy = OrderBy
+
   dashboard?: DashboardModel;
   private _taskStatusIdIndex?: number;   // the index for task_status id
 
+
+  private _orderBy: OrderBy = this.OrderBy.PRIORITY;
+
+  get orderBy() {
+    return this._orderBy;
+  }
+
+  set orderBy(value: OrderBy) {
+    this._orderBy = value;
+  }
+
+  private _orderReverse: boolean = true;
+
+  get orderReverse() {
+    return this._orderReverse;
+  }
+
+  set orderReverse(value: boolean) {
+    this._orderReverse = value;
+  }
 
   constructor(private dashboardService: DashboardService) {
   }
@@ -84,6 +113,37 @@ export class DashboardComponent {
 
   getAllTaskBasedOnStatusId(taskStatusId: number): TaskModel[] | undefined {
 
-    return this.dashboard?.tasks.filter(task => task.task_status_id === taskStatusId);
+    let tasksBasedOnStatusId = this.dashboard?.tasks.filter(task => task.task_status_id === taskStatusId);
+
+    tasksBasedOnStatusId?.sort((a: TaskModel, b: TaskModel): number => {
+
+      const aField: any = a[this.orderBy];
+      const bField: any = b[this.orderBy];
+
+      let res: number = 0;
+
+      if(aField > bField) {
+        res = 1;
+      }
+
+      if(aField < bField) {
+        res = -1;
+      }
+
+      // null and undefiend field as last
+      if(aField === undefined || aField === null)
+        res = 1;
+
+      if(bField === undefined || bField === null)
+        res = -1;
+
+      if(this.orderReverse)
+        return res * -1;
+
+
+      return res;
+    })
+
+    return tasksBasedOnStatusId;
   }
 }
