1e3fb75552a6f58b653500994d26b3959f39d884
ncla <ricciardincl@gmail.com>
2023-05-09 10:50:17 +0200

    Test

--- lib/repo/repo.py
+++ lib/repo/repo.py
@@ -1,18 +1,41 @@
 import git
 from dataclasses import dataclass
+from lib.utils.logger import Logger
+from datetime import datetime
 
 
-# @dataclass
-# class RepoNode:
-#     binsha: str
-#     author:
+@dataclass
+class Author:
+    email: str
+    name: str
+
+@dataclass
+class RepoNode:
+    hexsha: str
+    author: Author
+    message: str
+    committed_date: datetime
 
 
 class RepoManager:
     def __init__(self, repo_path: str, verbose: bool = False):
         self.repo_path = repo_path
         self.verbose = verbose
+        self.repo = git.Repo(self.repo_path)
+
+    def generate_tree(self):
+        commits = list(self.repo.iter_commits(reverse=True))
+
+        for commit in commits:
+            print(commit.hexsha)
+            print(commit.message)
+            print(commit.parents)
+            print(commit)
+            print("---")
 
 
 if __name__ == '__main__':
-    print(git)
+    path = "/home/ncla/Desktop/data/uni/programmazione-ad-oggetti/project/test/repo-test"
+    repo_manager = RepoManager(path, True)
+
+    repo_manager.generate_tree()
