ae89ee1a8456bb55308a7b92dc26124cc5a9a063
ncla <ricciardincl@gmail.com>
2023-05-13 14:12:09 +0200

    Updated help

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from lib.app.app import AppManager
 import sys
 import colorama
+from colorama import Fore
 from lib.utils.logger import Logger
 from lib.utils.utils import Utils
 from typing import Dict, List
@@ -35,7 +36,7 @@ def print_help() -> None:
     :return:
     """
 
-    app_name = """\
+    app_name = f"""{Fore.BLUE}\
  ███████████                   █████                          
 ░█░░░███░░░█                  ░░███                           
 ░   ░███  ░   ██████    █████  ░███ █████ █████ ████ ████████ 
@@ -47,19 +48,19 @@ def print_help() -> None:
                                                      ░███     
                                                      █████    
                                                     ░░░░░      
-"""
+{Fore.RESET}"""
 
     print(app_name)
 
-    msg: str = """
-run, r or nothing: launch the application
-demo, d: launch application with a demo database in path specified, path has to the last parameter
-  -f: force erase if there is already a database
-  -o: open app at end
-init, i: initialize this app in users projects
-  -f: force reinitialization
-help, h: print help 
-version, v: print version
+    msg: str = f"""\
+{Fore.GREEN}run{Fore.RESET}, {Fore.GREEN}r{Fore.RESET} or {Fore.CYAN}nothing{Fore.RESET}: launch the application
+{Fore.GREEN}demo{Fore.RESET}, {Fore.GREEN}d{Fore.RESET} [FLAGs] <path>: launch application with a demo database in path specified, path has to the last parameter
+  {Fore.MAGENTA}-f{Fore.RESET}: force erase if there is already a database
+  {Fore.MAGENTA}-o{Fore.RESET}: open app at end
+{Fore.GREEN}init, {Fore.GREEN}i{Fore.RESET}: initialize this app in users projects
+  {Fore.MAGENTA}-f{Fore.RESET}: force reinitialization
+{Fore.GREEN}help{Fore.RESET}, {Fore.GREEN}h{Fore.RESET}: print help 
+{Fore.GREEN}version{Fore.RESET}, {Fore.GREEN}v{Fore.RESET}: print version
 """
 
     print(msg)
